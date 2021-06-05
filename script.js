b7edc8a6bd82909ce015cb23464f048eb6eebb1c
thiagoteixas <thiagoteixas@gmail.com>
2021-06-05 18:11:07 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -17,9 +17,15 @@ window.onload = function createBoard() {
 function selectColor(button, currentColor) {
   // Retorna cor selecionada
   const selectedColor = button;
+  console.log(button);
   currentColor.classList.remove('selected');
   button.classList.add('selected');
   return selectedColor;
+}
+
+function colorPixels(event) {
+  console.log(event.target.style);
+  event.target.style.backgroundColor = currentRGB;
 }
 
 function teste(event) {
@@ -31,39 +37,43 @@ const button1 = document.getElementById('color1');
 const button2 = document.getElementById('color2');
 const button3 = document.getElementById('color3');
 const button4 = document.getElementById('color4');
-const pixels = document.getElementsByClassName('pixel');
+const pixels = document.getElementById('pixel-board');
 
-console.log(pixels);
+// Cor dos botões
+const firstButtonColor = window.getComputedStyle(button1, null).getPropertyValue('background-color');
+const secondButtonColor = window.getComputedStyle(button2, null).getPropertyValue('background-color');
+const thirdButtonColor = window.getComputedStyle(button3, null).getPropertyValue('background-color');
+const fourthButtonColor = window.getComputedStyle(button4, null).getPropertyValue('background-color');
 
 // Adiciona a classe selected para button1
 button1.classList.add('selected');
 
 // Variavel corAtual
 let currentColor = document.getElementsByClassName('selected')[0];
+let currentRGB = firstButtonColor;
 
 // Eventos de clique
 button1.addEventListener('click', function () {
   currentColor = selectColor(this, currentColor);
+  currentRGB = firstButtonColor;
   console.log(currentColor);
 });
 button2.addEventListener('click', function () {
   currentColor = selectColor(this, currentColor);
+  currentRGB = secondButtonColor;
   console.log(currentColor);
 });
 button3.addEventListener('click', function () {
   currentColor = selectColor(this, currentColor);
+  currentRGB = thirdButtonColor;
   console.log(currentColor);
 });
 button4.addEventListener('click', function () {
   currentColor = selectColor(this, currentColor);
+  currentRGB = fourthButtonColor;
   console.log(currentColor);
 });
-for (let index = 0; index < pixels.length; index += 1) {
-  pixels[index].addEventListener('click', function (currentColor) {
-    pixels[index].style.backgroundColor = currentColor.style.backgroundColor;
-    console.log(pixels[index]);
-  });
-}
+pixels.addEventListener('click', colorPixels);
 
 // Atualiza corAtual quando os botões forem clicados
 // corAtual = selectColor();
